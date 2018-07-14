666eb6c49585659786c05df91e0347aeb9083559
Dovydas Navickas <dovydas.nav@gmail.com>
2018-07-14 20:15:54 +0900

    Fixed Video props.

--- packages/sdk-react/src/components/video.tsx
+++ packages/sdk-react/src/components/video.tsx
@@ -2,113 +2,84 @@ import * as React from "react";
 import { NoiaClientContainer } from "@noia-network/sdk";
 import * as mimeTypes from "mime-types";
 
-
 import { Helpers } from "../utils/helpers";
 
-interface VideoHtmlPropsFix
-  extends React.DetailedHTMLProps<
-      React.ImgHTMLAttributes<HTMLVideoElement>,
-      HTMLVideoElement
-    > {
-  ref?: any;
+interface VideoHtmlPropsFix extends React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement> {
+    ref?: any;
 }
 
 export interface VideoProps extends VideoHtmlPropsFix {
-  src: string;
-  additionalSources?: HTMLSourceElement[];
-  /**
-   * Shows loader component when image is not loaded.
-   */
-  loaderComponent?: JSX.Element;
-  mimeType?: string;
-  sourceProps?: React.DetailedHTMLProps<
-    React.ImgHTMLAttributes<HTMLSourceElement>,
-    HTMLSourceElement
-  >;
-  ref?: React.Ref<Video>;
+    src: string;
+    additionalSources?: HTMLSourceElement[];
+    /**
+     * Shows loader component when image is not loaded.
+     */
+    loaderComponent?: JSX.Element;
+    mimeType?: string;
+    sourceProps?: React.DetailedHTMLProps<React.SourceHTMLAttributes<HTMLSourceElement>, HTMLSourceElement>;
+    ref?: React.Ref<Video>;
 }
 
 export interface VideoState {
-  base64?: string;
-  mimeType?: string;
+    base64?: string;
+    mimeType?: string;
 }
 
 export class Video extends React.Component<VideoProps, VideoState> {
-  constructor(props: VideoProps) {
-    super(props);
-    this.state = Video.getDerivedStateFromProps(props, {});
-  }
-
-  public element: HTMLVideoElement | null = null;
-  public sourceElement: HTMLSourceElement | null = null;
-
-  public static getDerivedStateFromProps(
-    nextProps: VideoProps,
-    prevState: VideoState
-  ): VideoState {
-    let mimeType: string | undefined;
-    if (nextProps.mimeType != null) {
-      mimeType = nextProps.mimeType;
-    } else {
-      mimeType = mimeTypes.lookup(nextProps.src) || undefined;
+    constructor(props: VideoProps) {
+        super(props);
+        this.state = Video.getDerivedStateFromProps(props, {});
     }
 
-    return {
-      ...prevState,
-      mimeType: mimeType
-    };
-  }
+    public element: HTMLVideoElement | null = null;
+    public sourceElement: HTMLSourceElement | null = null;
 
-  protected getProps(
-    props: VideoProps
-  ): React.DetailedHTMLProps<
-    React.ImgHTMLAttributes<HTMLVideoElement>,
-    HTMLVideoElement
-  > {
-    const {
-      src,
-      mimeType,
-      loaderComponent,
-      ref,
-      sourceProps,
-      ...restProps
-    } = props;
-    return restProps;
-  }
+    public static getDerivedStateFromProps(nextProps: VideoProps, prevState: VideoState): VideoState {
+        let mimeType: string | undefined;
+        if (nextProps.mimeType != null) {
+            mimeType = nextProps.mimeType;
+        } else {
+            mimeType = mimeTypes.lookup(nextProps.src) || undefined;
+        }
 
-  public async componentDidMount(): Promise<void> {
-    const bytes = await NoiaClientContainer.getClient().download({
-      src: this.props.src
-    });
-
-    this.setState({
-      base64: Helpers.bytesToBase64(bytes)
-    });
-  }
-
-  public render(): JSX.Element | null {
-    if (this.props.loaderComponent == null && this.state.base64 == null) {
-      return null;
-    } else if (
-      this.props.loaderComponent != null &&
-      this.state.base64 == null
-    ) {
-      return this.props.loaderComponent;
+        return {
+            ...prevState,
+            mimeType: mimeType
+        };
     }
 
-    return (
-      <video
-        ref={component => (this.element = component)}
-        {...this.getProps(this.props)}
-      >
-        <source
-          type={this.state.mimeType}
-          ref={component => (this.sourceElement = component)}
-          src={`data:${this.state.mimeType};base64,${this.state.base64}`}
-          {...this.props.sourceProps}
-        />
-        {this.props.additionalSources}
-      </video>
-    );
-  }
+    protected getProps(props: VideoProps): React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement> {
+        const { src, mimeType, loaderComponent, ref, sourceProps, ...restProps } = props;
+        return restProps;
+    }
+
+    public async componentDidMount(): Promise<void> {
+        const bytes = await NoiaClientContainer.getClient().download({
+            src: this.props.src
+        });
+
+        this.setState({
+            base64: Helpers.bytesToBase64(bytes)
+        });
+    }
+
+    public render(): JSX.Element | null {
+        if (this.props.loaderComponent == null && this.state.base64 == null) {
+            return null;
+        } else if (this.props.loaderComponent != null && this.state.base64 == null) {
+            return this.props.loaderComponent;
+        }
+
+        return (
+            <video ref={component => (this.element = component)} {...this.getProps(this.props)}>
+                <source
+                    type={this.state.mimeType}
+                    ref={component => (this.sourceElement = component)}
+                    src={`data:${this.state.mimeType};base64,${this.state.base64}`}
+                    {...this.props.sourceProps}
+                />
+                {this.props.additionalSources}
+            </video>
+        );
+    }
 }
