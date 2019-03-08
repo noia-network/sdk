97119304da3f4bf6198f97486a21f78007e83500
Danielius Dvareckas <danielius@noia.network>
2019-03-08 12:59:37 +0000

    Merged PR 518: SDK Web Library
    Related work items: #383

---
+++ packages/sdk-web-lib/README.md
@@ -0,0 +1,83 @@
+# NOIA Network SDK Web Library (SDK WL)
+
+## Building SDK WL
+
+1.  Clone this repository.
+
+```sh
+git clone https://github.com/noia-network/sdk.git
+```
+
+2.  Open `./packages/sdk` directory
+
+```sh
+$ cd packages/sdk
+```
+
+3. Install SDK dependencies
+
+```sh
+$ npm install
+```
+
+4.  Build SDK library
+
+```sh
+$ npm run build
+```
+
+5. Open `./packages/sdk-web-lib` directory
+
+6. Install SDK WL dependencies
+
+```sh
+$ npm install
+```
+
+7.  Build SDK WL library
+
+```sh
+$ npm run build
+```
+
+8.  Use library (`packages/sdk-web-lib/dist`)
+
+## Minimal `index.html` demo how SDK WL can be used
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta content="ie=edge" http-equiv="x-ua-compatible" />
+        <base href="/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Webpack App</title>
+    </head>
+    <body>
+        <div id="root"></div>
+        <script src="vendors~main.bundle.js" type="text/javascript"></script>
+        <script src="main.bundle.js" type="text/javascript"></script>
+        <img data-src="https://noia.network/wp-content/uploads/2018/03/icon.jpg" />
+    </body>
+    <script>
+        const ready = fn => {
+            if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+                fn();
+            } else {
+                document.addEventListener("DOMContentLoaded", fn);
+            }
+        };
+
+        // Custom master url (optional).
+        // window.NOIA.settings.masterUrl = "wss://csl-masters.noia.network:5566";
+
+        // Enable/disable logging (optional).
+        // window.NOIA.settings.log = true;
+        window.NOIA.init();
+        ready(() => {
+            window.NOIA.load();
+        });
+    </script>
+</html>
+```
---
+++ packages/sdk-web-lib/src/index.ts
@@ -0,0 +1,68 @@
+import "../../sdk/dist/vendors~main";
+import { NoiaClient, NoiaClientContainer } from "../../sdk";
+import * as PieceWorker from "worker-loader!../../sdk/worker";
+import * as Sha1Worker from "worker-loader!rusha/dist/rusha";
+
+import { LoggerBuilder, LogLevel, ConsoleMessageHandler } from "simplr-logger";
+import { bytesToBase64 } from "./base64";
+
+const settings: { masterUrl?: string; log?: boolean } = {};
+
+const logger = new LoggerBuilder({
+    DefaultLogLevel: {
+        LogLevel: LogLevel.Trace,
+        LogLevelIsBitMask: false
+    },
+    WriteMessageHandlers: [
+        {
+            Handler: new ConsoleMessageHandler(),
+            LogLevel:
+                settings.log === true
+                    ? LogLevel.Critical | LogLevel.Debug | LogLevel.Information | LogLevel.Error | LogLevel.Trace | LogLevel.Warning
+                    : LogLevel.None,
+            LogLevelIsBitMask: true
+        }
+    ]
+});
+
+const init = () => {
+    NoiaClientContainer.initialize(
+        new NoiaClient({
+            masterUrl: typeof settings.masterUrl === "string" ? settings.masterUrl : "wss://csl-masters.noia.network:5566",
+            logger: logger,
+            pieceWorkerConstructor: () => new PieceWorker(),
+            sha1WorkerConstructor: () => new Sha1Worker()
+        })
+    );
+};
+
+function load(): void {
+    const images = document.getElementsByTagName("img");
+    const imageType = "image/jpeg";
+    const loadImage = async (img: HTMLImageElement): Promise<void> => {
+        const imgDataSrc = img.getAttribute("data-src");
+        if (imgDataSrc) {
+            //@ts-ignore
+            const stream = await window.NOIA.clientContainer.getClient().openStream({
+                src: imgDataSrc
+            });
+
+            //Image came from CDN.
+            if (stream.masterData.metadata) {
+                // Load image bytes.
+                const imageBytes = await stream.getAllBytes();
+                // Render image as bytes.
+                img.src = `data:${imageType};base64,${bytesToBase64(imageBytes)}`;
+                logger.Info(`Image ${stream.masterData.src}: downloaded (${stream.masterData.metadata.bufferLength} bytes).`);
+            } else {
+                img.src = imgDataSrc;
+                logger.Warn(`Image ${stream.masterData.src}: not found.`);
+            }
+        }
+    };
+    for (let i = 0; i < images.length; i++) {
+        loadImage(images[i]);
+    }
+}
+
+module.exports = { NOIA: { init, clientContainer: NoiaClientContainer, load, settings } };
